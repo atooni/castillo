1086bdb409b71a2727c5f33a6841770016579fa8
Andreas Gies <andreas@wayofquality.de>
2015-01-16 21:19:30 +0100

    Adding Portfolio service implementation in memory only.

---
+++ server/app/de/woq/castillo/services/PortfolioImpl.scala
@@ -0,0 +1,28 @@
+package de.woq.castillo.services
+
+import java.util.concurrent.atomic.AtomicLong
+
+import de.woq.castillo.model.{SeminarDetails, Seminar}
+
+import scala.collection.concurrent.TrieMap
+
+object PortfolioImpl extends Portfolio {
+
+  private[this] val seqId    = new AtomicLong
+  private[this] val seminars = TrieMap.empty[Long, Seminar]
+
+  override def list(): Seq[Seminar] = seminars.values.to[Seq]
+
+  override def create(s : SeminarDetails) : Option[Seminar] = {
+    val id = seqId.incrementAndGet()
+    val seminar = Seminar(id, s)
+    seminars.put(id, seminar)
+    Some(seminar)
+  }
+
+  override def update(seminar : Seminar) = seminars.replace(seminar.id, seminar.copy())
+
+  override def delete(id: Long): Option[Seminar] = seminars.remove(id)
+
+  override def get(id: Long): Option[Seminar] = seminars.get(id)
+}
