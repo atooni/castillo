f824d6c0f7cc2166e3a2eed339532aec8b6d1927
Andreas Gies <andreas@wayofquality.de>
2015-01-17 10:40:43 +0100

    Adding routes for the Portfolio REST service.

---
+++ server/app/Global.scala
@@ -0,0 +1,21 @@
+import de.woq.castillo.model.SeminarDetails
+import de.woq.castillo.services.{PortfolioImpl, Portfolio}
+import play.api.{Application, GlobalSettings}
+
+object Global extends GlobalSettings {
+  
+  val portfolio : Portfolio = PortfolioImpl
+
+  override def onStart(app: Application): Unit = {
+    super.onStart(app)
+    
+    if (portfolio.list().isEmpty) {
+      portfolio.create(SeminarDetails(
+        title = "Creating Play applications with a ScalaJS frontend",
+        description = "The title says it all",
+        trainer = "andreas@wayofquality.de",
+        duration = 5
+      ))
+    }
+  }
+}
