d1bab10b6030f1d588caa66c41f6a2d919cca34d
Andreas Gies <andreas@wayofquality.de>
2015-01-18 15:48:56 +0100

    Refactoring HTTP based tests.

--- server/test/de/woq/castillo/app/PortfolioRestSpec.scala
+++ server/test/de/woq/castillo/app/PortfolioRestSpec.scala
@@ -6,7 +6,7 @@ import org.scalatest.{Matchers, OptionValues, WordSpec}
 import org.scalatestplus.play.{OneServerPerSuite, WsScalaTestClient}
 import play.api.http.Writeable
 import play.api.libs.json._
-import play.api.libs.ws.WS
+import play.api.libs.ws.{WSRequestHolder, WS}
 import play.mvc.Http.Status._
 
 import scala.concurrent.Await
@@ -31,112 +31,106 @@ class PortfolioRestSpec extends WordSpec
   implicit private def jsWriteable[A](
     implicit wa: Writes[A], wjs: Writeable[JsValue]
   ): Writeable[A] = wjs.map(a => Json.toJson(a))
+  
+  private def executeWsRequest(request : WSRequestHolder) =  
+    Await.result(request.execute(), 1.second)
+  
+  private def createSeminar(details : SeminarDetails) = {
+    
+    val result = executeWsRequest(
+      WS.url(portfolioBase)
+        .withBody[SeminarDetails](details)
+        .withMethod("POST")
+    )
+
+    result.status should be (OK)
+    validateJSON[Seminar](result.body)
+  }
+  
+  private def validateJSON[T](json : String)(implicit reads: Reads[T]) : Option[T] = {
+    val result = Json.fromJson[T](Json.parse(json)).asOpt
+    
+    result match { 
+      case None => fail("Failed to validate JSON response")
+      case Some(r) => Some(r)
+    }
+    
+    result
+  }
 
   "The portfolio REST service" should {
     
     "provide the list of available seminars at /portfolio" in {
-      val result = Await.result(WS.url(portfolioBase).get(), 1.second)
+      val result = executeWsRequest(WS.url(portfolioBase).withMethod("GET"))
       result.status should be (OK)
-
-      val seminars = Json.fromJson[Seq[Seminar]](Json.parse(result.body))
-      seminars.isSuccess should be (true)
-
-      seminars.get should have size(1)
+      val seminars = validateJSON[Seq[Seminar]](result.body) 
+      seminars.foreach(_ should have size(1))
     }
     
     "allow to retrieve an existing seminar" in {
-      val result = Await.result(WS.url(s"$portfolioBase/1").get(), 1.second)
+      val result = executeWsRequest(WS.url(s"$portfolioBase/1").withMethod("GET"))
       result.status should be (OK)
       
-      val seminar = Json.fromJson[Seminar](Json.parse(result.body))
-      seminar.isSuccess should be (true)
+      val seminar = validateJSON[Seminar](result.body)
+      seminar should not be (None)
       
-      seminar.get should be (Seminar(1, SeminarDetails(
+      seminar.foreach(_ should be (Seminar(1, SeminarDetails(
         title = "Creating Play applications with a ScalaJS frontend",
         description = "The title says it all",
         trainer = "andreas@wayofquality.de",
         duration = 5
-      )))
+      ))))
     } 
     
     "respond with a HTTP NotFound when trying to retrieve a non-existing seminar" in {
-      val result = Await.result(WS.url(s"$portfolioBase/${Long.MaxValue}").get(), 1.second)
-      result.status should be (NOT_FOUND)
+      executeWsRequest(WS.url(s"$portfolioBase/${Long.MaxValue}").withMethod("GET")).status should be (NOT_FOUND)
     }
     
     "allow to create a seminar" in {
-
-      val result = Await.result(
-        WS.url(portfolioBase)
-          .withBody[SeminarDetails](details)
-          .withMethod("POST")
-          .execute(),
-        1.second
-      )
-      
-      result.status should be (OK)
-
-      val seminar = Json.fromJson[Seminar](Json.parse(result.body))
-      seminar.isSuccess should be (true)
-      seminar.get.details should be (details)
+      createSeminar(details) should not be (None)
     }
     
     "fail to create a seminar with wrong details" in {
 
-      val result = Await.result(
+      executeWsRequest(
         WS.url(portfolioBase)
           .withBody[SeminarDetails](details.copy(duration = 0))
           .withMethod("POST")
-          .execute(),
-        1.second
-      )
-
-      result.status should be (BAD_REQUEST)
+      ).status should be (BAD_REQUEST)
     }
     
     "allow to delete an object by it's id" in {
       
-      val result = Await.result(WS.url(s"$portfolioBase/1").delete(), 1.second)
+      val result = executeWsRequest(WS.url(s"$portfolioBase/1").withMethod("DELETE"))
 
       result.status should be (OK)
       
       val seminar = Json.fromJson[Seminar](Json.parse(result.body))
       seminar.isSuccess should be (true)
-      seminar.get.id should be (1)
+      seminar.foreach(_.id should be (1))
 
-      Await.result(WS.url(s"$portfolioBase/1").get(), 1.second).status should be (NOT_FOUND)
+      executeWsRequest(WS.url(s"$portfolioBase/1").withMethod("GET")).status should be (NOT_FOUND)
     }
     
     "return HTTP 404 when trying to delete a non-existing seminar" in {
-
-      Await.result(WS.url(s"$portfolioBase/${Long.MaxValue}").delete(), 1.second).status should be (NOT_FOUND)
+      executeWsRequest(WS.url(s"$portfolioBase/${Long.MaxValue}").withMethod("DELETE")).status should be (NOT_FOUND)
     }
     
     "allow to update an existing seminar" in {
-      val result = Await.result(
-        WS.url(portfolioBase)
-          .withBody[SeminarDetails](details)
-          .withMethod("POST")
-          .execute(),
-        1.second
-      )
+      createSeminar(details) match {
+        case None => fail("Seminar for update not created")
+        case Some(s) =>
+          val update = executeWsRequest(
+            WS.url(s"$portfolioBase/${s.id}")
+              .withBody[Seminar](s.copy(details = details.copy(duration = 7)))
+              .withMethod("PUT")
+          )
 
-      result.status should be (OK)
-
-      val seminar = Json.fromJson[Seminar](Json.parse(result.body))
-      seminar.isSuccess should be (true)
-      seminar.get.details should be (details)
-      
-      val update = Await.result(
-        WS.url(s"$portfolioBase/${seminar.get.id}").put[Seminar](
-          seminar.get.copy(details = details.copy(duration = 7))
-        ), 1.second
-      )
-      
-      update.status should be (OK)
-      val updated = Json.fromJson[Seminar](Json.parse(update.body))
-      updated.isSuccess should be (true)
-      updated.get.details should be (details.copy(duration = 7))
+          update.status should be (OK)
+          val updated = Json.fromJson[Seminar](Json.parse(update.body))
+          updated.isSuccess should be (true)
+          updated.get.details should be (details.copy(duration = 7))
+      }
     }
 
     "fail if updating the seminar with wrong details" in {
