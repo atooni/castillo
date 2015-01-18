91861f1788044715997db03f5482c6d517704302
Andreas Gies <andreas@wayofquality.de>
2015-01-18 21:44:47 +0100

    Updating build script.

--- build.sh
+++ build.sh
@@ -1,6 +1,21 @@
 #!/bin/sh
+
+function checkRC {
+  if [ $1 != 0 ]; then 
+    printf "Build failed! ($1)\n"
+    exit $1
+  fi  
+}
+
 sbt clean coverage test
+checkRC $?
+
 sbt coverageReport
+checkRC $?
+
 sbt coverageAggregate
+checkRC $?
+
 sbt codacyCoverage
+checkRC $?
 
